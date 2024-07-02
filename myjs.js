7d7367d0b7c6bb069bda92f54451d97f4d45974d
migitakaren <168495203+migitakaren@users.noreply.github.com>
2024-07-02 10:18:25 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,8 +1,12 @@
 alert('Hello, Karen!');
 alert(1+1);
 alert(Math.sprt(1+1));
-2+3
-'2'+'3'
-Number('2')+Number('3')
-'2'+3
-Number('2')+3
+alert(Math.sprt(2+3));
+alert(Math.sprt(1/0));
+alert(Math.sprt(0/0));
+alert(Math.sprt(2+3));
+alert(Math.sprt('2'+'3'));
+alert(Math.sprt(Number('2')+Number('3')));
+alert(Math.sprt('2'+3));
+alert(Math.sprt(Number('2')+3));
+
